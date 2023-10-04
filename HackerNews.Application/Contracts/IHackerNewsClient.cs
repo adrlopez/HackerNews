4225edc44a6882019690961830cda9bf5e705f3f
adrlopez <adr.lopez@gmail.com>
2023-10-04 13:03:22 -0300

    Created main projects, also a couple of CQRS queries

---
+++ HackerNews.Application/Contracts/IHackerNewsClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackerNews.Domain;
+
+namespace HackerNews.Application.Contracts
+{
+    public interface IHackerNewsClient
+    {
+        Task<List<int>> GetNewStories();
+
+        Task<Story> GetStoryById(int id);
+    }
+}
---
+++ HackerNews.Application/MappingProfiles/StoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HackerNews.Application.Features.Story.Queries.GetStoryDetails;
+using HackerNews.Domain;
+
+namespace HackerNews.Application.MappingProfiles
+{
+    public class StoryProfile : Profile
+    {
+        public StoryProfile()
+        {
+            CreateMap<StoryDto, Story>().ReverseMap();
+        }
+    }
+}
